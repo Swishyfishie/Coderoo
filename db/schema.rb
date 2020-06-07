3a053afeb965209374887a679701c37f2ff41b7d
Swishyfishie2 <pclandreiuk@gmail.com>
2020-06-08 00:37:26 +0100

    added bycrypt and password to the user

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2020_06_07_220543) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "date_started"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
